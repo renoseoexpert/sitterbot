225ac7aa8a74dd71c8778318d92d10c4f38b7354
CJ Avilla <cjavilla@gmail.com>
2016-11-19 09:20:35 -0800

    add models

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119072955) do
+ActiveRecord::Schema.define(version: 20161119171907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.boolean  "food_included", default: false
+    t.datetime "starts_at",                     null: false
+    t.datetime "ends_at",                       null: false
+    t.string   "key",                           null: false
+    t.integer  "sitter_id",                     null: false
+    t.integer  "parent_id",                     null: false
+    t.integer  "flat_rate"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+  end
+
+  add_index "events", ["parent_id"], name: "index_events_on_parent_id", using: :btree
+  add_index "events", ["sitter_id"], name: "index_events_on_sitter_id", using: :btree
+
+  create_table "kids", force: :cascade do |t|
+    t.integer  "parent_id",    null: false
+    t.string   "nickname",     null: false
+    t.date     "birthdate"
+    t.text     "instructions"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "kids", ["parent_id"], name: "index_kids_on_parent_id", using: :btree
+
+  create_table "sitters", force: :cascade do |t|
+    t.integer  "parent_id",                    null: false
+    t.boolean  "paid",          default: true
+    t.integer  "hourly_rate",   default: 10
+    t.string   "phone"
+    t.string   "email"
+    t.string   "name",                         null: false
+    t.boolean  "can_drive",     default: true
+    t.integer  "per_extra_kid"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "sitters", ["parent_id"], name: "index_sitters_on_parent_id", using: :btree
 
   create_table "ssl_verifications", force: :cascade do |t|
     t.string   "key"
